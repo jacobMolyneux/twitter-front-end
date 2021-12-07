fb75259719bff0f656d6358f663fd824a394456f
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-12-06 20:32:30 -0500

    added the ability to create new tweets and I also added a function to map the list of tweets to tweet cards to be displayed on home page

--- src/pages/components/tweetCard.js
+++ src/pages/components/tweetCard.js
@@ -1,11 +1,23 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
 
-const TweetCard = () => {
+const TweetCard = (props) => {
   return (
-    <Card>
-      <Card.Title>Header</Card.Title>
-      <Card.Text>This is card text</Card.Text>
+    <Card className="p-3">
+      <Card.Title>{props.username}</Card.Title>
+      <Card.Text>{props.text}</Card.Text>
+      <Row className="d-flex">
+        <ListGroup horizontal className="d-flex justify-content-around">
+          <ListGroup.Item style={{ height: "10%" }} className="border-0">
+            Like
+          </ListGroup.Item>
+          <ListGroup.Item style={{ height: "10%" }} className="border-0">
+            Dislike
+          </ListGroup.Item>
+        </ListGroup>
+      </Row>
     </Card>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,34 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { TweetCard } from "./components/tweetCard";
 
 const Homepage = () => {
   let [newTweet, setNewTweet] = useState("");
+  let [tweetList, setTweetList] = useState([
+    "I am Iron Man",
+    "HULK SMASH",
+    "Hello World",
+  ]);
+  const createTweet = () => {
+    setTweetList(tweetList.concat(newTweet));
+    setNewTweet((newTweet = ""));
+  };
   return (
-    <Container>
-      <Col>Tweets go here</Col>
+    <Container style={{ width: "60%" }}>
+      <Form>
+        <Form.Control
+          as="textarea"
+          onChange={(e) => setNewTweet((newTweet = e.target.value))}
+        ></Form.Control>
+        <Button onClick={createTweet}>Tweet</Button>
+      </Form>
+      <Col>
+        {tweetList.map((tweet) => (
+          <TweetCard username="user" text={tweet} />
+        ))}
+      </Col>
     </Container>
   );
 };
